335b9fac57d67fdaf0cc0b6dfbc33cda0e2ebd1c
Shawn Dsilva <shawn@shawndsilva.com>
2021-10-27 22:38:46 +0530

    front end development started

--- src/components/sidebar/country-select.js
+++ src/components/sidebar/country-select.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import 'react-dropdown/style.css';
 import Dropdown from 'react-dropdown'
+import co2_emissions from '../../json/carbon_dioxide_co2_emissions_without_land_use_land_use_change_and_forestry_lulucf_in_kilotonne_co2_equivalent.json'
 
 export default function CountrySelect() {
     //Write logic to render all countries as dropdown options
@@ -9,7 +10,7 @@ export default function CountrySelect() {
     ];
     
     const defaultOption = options[0];
-
+    console.log(co2_emissions);
     return(
         <div className="country-select">
             <Dropdown options={options} value={defaultOption} placeholder="Select an option" />
--- src/components/sidebar/parameter-select.js
+++ src/components/sidebar/parameter-select.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import 'react-dropdown/style.css';
 import Dropdown from 'react-dropdown'
 
 export default function ParameterSelect() {
     //Write logic to render all countries as dropdown options
     const options = [
-        'Parameter One', 'Parameter Two', 'Parameter Three'
+        'Carbon Dioxide (CO2)', 'Greenhouse Gases (GHG) Type 1', 'Greenhouse Gases (GHG) Type 2','Hydrofluorocarbons (HFC)', 'Methane (CH4)', 'Nitrogen Triflouride (NF3)', 'Nitrous Oxide (N2O)', 'Perflourocarbons (PFC)', 'Sulphur Hexaflouride (SF6)', 'Unspecified HFC & PFC Mix'
     ];
     const defaultOption = options[0];
 
+    const [option, setOption] = useState('Carbon Dioxide (CO2)');
+    const onSelect = (selection) => {
+        setOption(selection.value);
+    }
+
     return(
         <div className="parameter-select">
-            <Dropdown options={options} value={defaultOption} placeholder="Select an option" />
+            <label>Emission Type</label>
+            <Dropdown options={options} value={defaultOption} onChange={onSelect} placeholder="Select an option" />
+
+            <span>Your Selection : {option}</span>
         </div>
     )
 }
