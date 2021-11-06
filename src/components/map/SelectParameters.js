cb366a5af72c24ad83fdbabf7adc47b328c3b1b8
Shawn Dsilva <shawn@shawndsilva.com>
2021-11-06 17:11:35 +0530

    Filtering data for map visualization done

---
+++ src/components/map/SelectParameters.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+
+function SelectParameters({year, setYear, emissionType, setEmissionType, setEmissionData}) {
+
+    useEffect(() => {
+        const fetchEmissionData = () => {
+            let filteredData = [];
+
+            fetch(`./json/byEmission/${emissionType}.json`)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach( (country) => {
+                    for( const key in country) {
+                        let filteredDatum = country[key].filter( (yearValueObject) => {
+                            // let thisYear = Object.entries(yearValueObject)[0][0];
+                            // return parseInt(thisYear) === parseInt(year);
+                            return yearValueObject[year] !== undefined;
+                        })[0]
+
+                        if(filteredDatum !== undefined) {
+                            let countryObj = {[key]:filteredDatum[year]};
+                            filteredData.push(countryObj);
+                        }
+
+                    }
+                })
+                setEmissionData(filteredData);
+            });
+        }
+        fetchEmissionData();
+    },[])
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SelectParameters
--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
+import SelectParameters from './SelectParameters';
 import SvgGenerator from './SvgGenerator';
 // import {ReactComponent as ZaWarudo} from './svg/zawarudo.svg';
 
 export default function Map() {
 
+  const[ year, setYear] = useState(2014);
+  const[ emissionType, setEmissionType] = useState("carbon_dioxide_co2_emissions_without_land_use_land_use_change_and_forestry_lulucf_in_kilotonne_co2_equivalent")
+  const[ emissionData, setEmissionData] = useState([])
 
   return (
     //The code to render a map goes here.
     <div className="map">
       <h1>『 ZA WARUDO 』 </h1>
-<SvgGenerator/>
+      <SvgGenerator year={year} setYear={setYear} emissionType={emissionType} setEmissionType={setEmissionType} emissionData={emissionData} setEmissionData={setEmissionData}/>
+      <SelectParameters year={year} setYear={setYear} emissionType={emissionType} setEmissionType={setEmissionType} emissionData={emissionData} setEmissionData={setEmissionData}/>
     </div>
   );
 }
