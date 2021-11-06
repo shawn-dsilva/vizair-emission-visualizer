11e3d0fbc88e4d1d4a1f27ec64463d4ca628d6e1
Shawn Dsilva <shawn@shawndsilva.com>
2021-11-06 19:27:32 +0530

    No undefined error on first load

--- src/components/map/SvgGenerator.js
+++ src/components/map/SvgGenerator.js
@@ -1,62 +1,76 @@
 import React, { useEffect, useState } from 'react'
 import WorldMap from './svg/WorldMap.json';
 
-function SvgGenerator({emissionData}) {
+function SvgGenerator({emissionData, isLoading, setIsLoading}) {
 
     const [colorized, setColorized] = useState({});
-    const [isLoading, setIsLoading] = useState(false);
 
     const colors = [
-        '#fee5d9',
-        '#fcbba1',
-        '#fc9272',
-        '#fb6a4a',
-        '#de2d26',
-        '#a50f15'
+      '#fff5f0',
+      '#fee0d2',
+      '#fcbba1',
+      '#fc9272',
+      '#fb6a4a',
+      '#ef3b2c',
+      '#cb181d',
+      '#a50f15',
+      '#67000d',
+      '#420008'
     ]
+
+    const colorizeData = () => {
+      let colorized = {};
+
+      for(const key in emissionData) {
+          let val = emissionData[key];
+          switch (true) {
+            case val > 0 && val < 250000:
+              colorized[key] = {'value':val, 'color' : colors[0]}
+              break;
+            case val > 250000 && val < 300000:
+              colorized[key] = {'value':val, 'color' : colors[1]}
+              break;
+            case val > 350000 && val < 400000:
+              colorized[key] = {'value':val, 'color' : colors[2]}
+              break;
+            case val > 400000 && val < 450000:
+              colorized[key] = {'value':val, 'color' : colors[3]}
+              break;
+            case val > 450000 && val < 500000:
+              colorized[key] = {'value':val, 'color' : colors[4]}
+              break;
+            case val > 500000 && val < 1000000:
+              colorized[key] = {'value':val, 'color' : colors[5]}
+              break;
+            case val > 1000000 && val < 1500000:
+              colorized[key] = {'value':val, 'color' : colors[6]}
+              break;
+            case val > 1500000 && val < 2000000:
+              colorized[key] = {'value':val, 'color' : colors[7]}
+              break;
+            case val > 2000000 && val < 2500000:
+              colorized[key] = {'value':val, 'color' : colors[8]}
+              break;
+            default:
+              colorized[key] = {'value':val, 'color' : colors[9]}
+          }
+      }
+      setColorized(colorized);  
+      setIsLoading(false);
+
+  }
 
     useEffect(() => {
         colorizeData();
-    }, [isLoading])
+    }, [emissionData])
 
 
-    const colorizeData = () => {
-        setIsLoading(true);
-        let colorized = {};
-
-        for(const key in emissionData) {
-            let val = emissionData[key];
-            switch (true) {
-              case val > 0 && val < 500000:
-                colorized[key] = {'value':val, 'color' : colors[0]}
-                break;
-              case val > 500000 && val < 1000000:
-                colorized[key] = {'value':val, 'color' : colors[1]}
-                break;
-              case val > 1000000 && val < 1500000:
-                colorized[key] = {'value':val, 'color' : colors[2]}
-                break;
-              case val > 1500000 && val < 2000000:
-                colorized[key] = {'value':val, 'color' : colors[3]}
-                break;
-              case val > 2000000 && val < 2500000:
-                colorized[key] = {'value':val, 'color' : colors[4]}
-                break;
-              default:
-                colorized[key] = {'value':val, 'color' : colors[5]}
-            }
-        }
-
-
-        setColorized(colorized);  
-        
-        // setIsLoading(false);
-    }
+    
 
     const getSafeColor = (countryName) => {
       if(countryName === 'United States') {
         // Check for American Exceptionalism
-        return colorized['United States of America'].color;
+        return colorized['United States of America']?.color;
       } else if( colorized[countryName] === undefined) {
         // Return light grey hex code for countries that have no data
         return '#D3D3D3'
@@ -67,25 +81,29 @@ function SvgGenerator({emissionData}) {
       }
 
     const createChildElements = () => {
+
         let countries = WorldMap.children[2].children;
 
-        let elements = countries.map((country) => {
+        let elements = countries.map((country, index) => {
             let countryName = country.attributes["data-name"];
             console.log(countryName);
             console.log(colorized['United States of America']?.color);
-            return <path d={country.attributes.d} fill={getSafeColor(countryName)} dataid={country.attributes["data-id"]} dataname={country.attributes["data-name"]} id={country.attributes.id} />
+            return <path key={index} d={country.attributes.d} fill={getSafeColor(countryName)} dataid={country.attributes["data-id"]} dataname={country.attributes["data-name"]} id={country.attributes.id} />
         })
         return elements
+
     }
 
 
     return (
+      <>
+      { isLoading === true ? "LOADING" : (
         <svg enableBackground="new 0 0 1000 647" height="647px" pretty_print="False" style={{"strokeLineJoin": "round", "stroke":"#000", "fill": "none"}} version="1.1" viewBox="0 0 1000 647" width="1000px" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"><defs><style type="text/css"></style></defs><metadata><views><view h="647.825177808" padding="0" w="1000"><proj flip="auto" id="mercator" lon0="65.3146660706"/><bbox h="4064.12" w="6283.19" x="-3141.59" y="-2891.13"/></view></views></metadata><g class="" id="countries">
-
-            { isLoading && createChildElements()}
+         {createChildElements()}
             </g>
-        </svg>
-
+        </svg>)
+      }
+      </>
     )
 }
 
--- src/components/map/index.js
+++ src/components/map/index.js
@@ -8,13 +8,14 @@ export default function Map() {
   const[ year, setYear] = useState(2014);
   const[ emissionType, setEmissionType] = useState("carbon_dioxide_co2_emissions_without_land_use_land_use_change_and_forestry_lulucf_in_kilotonne_co2_equivalent")
   const[ emissionData, setEmissionData] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
 
   return (
     //The code to render a map goes here.
     <div className="map">
       <h1>『 ZA WARUDO 』 </h1>
-      <SvgGenerator year={year} setYear={setYear} emissionType={emissionType} setEmissionType={setEmissionType} emissionData={emissionData} setEmissionData={setEmissionData}/>
-      <SelectParameters year={year} setYear={setYear} emissionType={emissionType} setEmissionType={setEmissionType} emissionData={emissionData} setEmissionData={setEmissionData}/>
+      <SvgGenerator year={year} setYear={setYear} emissionType={emissionType} setEmissionType={setEmissionType} emissionData={emissionData} setEmissionData={setEmissionData} isLoading={isLoading} setIsLoading={setIsLoading}/>
+      <SelectParameters year={year} setYear={setYear} emissionType={emissionType} setEmissionType={setEmissionType} emissionData={emissionData} setEmissionData={setEmissionData} />
     </div>
   );
 }
